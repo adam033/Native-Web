b6d3f3c93531a419da33664461d1593089968fa9
keroco <53498716+adam033@users.noreply.github.com>
2020-03-25 22:17:42 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,6 +0,0 @@
-<html>
-    <body>
-    <script src="http://localhost/perpus/aset/jquery.js"></script>
-    <script src="http://localhost/perpus/asset/bootstrap/js/bootstrap.min.js"></script>  
-    </body>
-</html>
