d402a059095c7ea7f57d1ca974df5a044f2ac650
keroco <53498716+adam033@users.noreply.github.com>
2020-03-25 22:18:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+include 'aset/header.php';
+?>
+<style>
+    body{
+        background-color: lightgrey;
+    }
+    hr{
+        background-color: black;
+    }
+</style>
+<div class="container">
+        <div class="row mt-4">
+            <div class="col-md">
+                <h2><i class="fas fa-chart-line mr-2"></i>Dashboard</h2>
+                <hr class="bg-dark">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+            <div class="col-md-4">
+                <div class="card bg-info" style="width: 18rem;">
+                    <div class="card-body text-white">
+                        <h5 class="card-title">Jumlah Buku</h5>
+                        <p class="card-text" style="font-size:60px">562</p>
+                        <a href="#" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div> 
+            </div>
+
+                
+            </div>
+
+            <div class="col-md-4">
+            <div class="col-md-4">
+                <div class="card bg-primary" style="width: 18rem;">
+                    <div class="card-body text-white">
+                        <h5 class="card-title">Jumlah User</h5>
+                        <p class="card-text" style="font-size:60px">234</p>
+                        <a href="#" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div> 
+            </div>
+
+           
+            </div>
+            <div class="col-md-4">
+            <div class="col-md-4">
+                <div class="card bg-dark" style="width: 18rem;">
+                    <div class="card-body text-white">
+                        <h5 class="card-title">Jumlah Transaksi</h5>
+                        <p class="card-text" style="font-size:60px">314</p>
+                        <a href="#" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div> 
+            </div>
+
+                
+            </div>
+        </div>
+
+</div>
+
+
+<?php
+include 'aset/footer.php';
+?>
